7ec7e430981577968b0acb7fbdb00a85a37a456a
salman354 <mskhn2255@gmail.com>
2024-03-19 12:38:48 +0530

    requirement file added and url file updated

--- webdev/webdev/urls.py
+++ webdev/webdev/urls.py
@@ -8,10 +8,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'), 
-    path('mashel_ai/', views.mashel_ai, name='mashel_ai'),
-    path('chatbot/', views.chatbot, name='chatbot'), 
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register')
+    path('mashel_ai', views.mashel_ai, name='mashel_ai'),
+    path('chatbot', views.chatbot, name='chatbot'), 
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
